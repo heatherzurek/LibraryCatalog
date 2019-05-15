ae15a4c5c8cf53939914950909a6948d0bfe6c56
Heather Zurek and Megan Schulte <megan@>
2019-05-15 16:35:54 -0700

    export databases

--- library_catalog_test.sql
+++ library_catalog_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 15, 2019 at 04:41 PM
+-- Generation Time: May 15, 2019 at 11:35 PM
 -- Server version: 5.6.38
 -- PHP Version: 7.2.1
 
@@ -40,17 +40,17 @@ CREATE TABLE `authors` (
 --
 
 CREATE TABLE `books` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `catalog`
+-- Table structure for table `catalogs`
 --
 
-CREATE TABLE `catalog` (
+CREATE TABLE `catalogs` (
   `id` int(11) NOT NULL,
   `book_id` int(11) NOT NULL,
   `author_id` int(11) NOT NULL
@@ -59,12 +59,15 @@ CREATE TABLE `catalog` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `checked_out`
+-- Table structure for table `checkouts`
 --
 
-CREATE TABLE `checked_out` (
+CREATE TABLE `checkouts` (
   `id` int(11) NOT NULL,
-  `available` tinyint(1) NOT NULL
+  `copies_id` int(11) NOT NULL,
+  `patrons_int` int(11) NOT NULL,
+  `due_date` date NOT NULL,
+  `is_due` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -75,7 +78,8 @@ CREATE TABLE `checked_out` (
 
 CREATE TABLE `copies` (
   `id` int(11) NOT NULL,
-  `number_of_copies` int(11) NOT NULL
+  `book_id` int(11) NOT NULL,
+  `count` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -86,7 +90,7 @@ CREATE TABLE `copies` (
 
 CREATE TABLE `patrons` (
   `id` int(11) NOT NULL,
-  `patron_name` varchar(255) NOT NULL
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -100,15 +104,21 @@ ALTER TABLE `authors`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `catalog`
+-- Indexes for table `books`
 --
-ALTER TABLE `catalog`
+ALTER TABLE `books`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `checked_out`
+-- Indexes for table `catalogs`
 --
-ALTER TABLE `checked_out`
+ALTER TABLE `catalogs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `checkouts`
+--
+ALTER TABLE `checkouts`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -131,18 +141,24 @@ ALTER TABLE `patrons`
 -- AUTO_INCREMENT for table `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `catalog`
+-- AUTO_INCREMENT for table `books`
 --
-ALTER TABLE `catalog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `checked_out`
+-- AUTO_INCREMENT for table `catalogs`
 --
-ALTER TABLE `checked_out`
+ALTER TABLE `catalogs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `checkouts`
+--
+ALTER TABLE `checkouts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
